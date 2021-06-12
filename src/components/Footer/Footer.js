fb58f1c001400551c98a97e78ffd7612ebf1255c
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-12 21:19:08 +0900

    top 선 추가

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-
 const FooterContainer = styled.div`
     display: flex;
     justify-content: center;
@@ -9,6 +8,7 @@ const FooterContainer = styled.div`
     padding: 0.5rem;
     border: 1px solid white;
     height: 5%;
+    border-top: 1px solid #C2B7DA;
 `;
 
 const Footer = () => {
