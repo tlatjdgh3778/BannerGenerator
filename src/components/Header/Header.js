de204f19441e223db8ea2bb5db01570f15744153
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-12 21:19:25 +0900

    bottom 선 추가, padding 조정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,12 @@ import HeaderTitle from 'components/Header/HeaderTitle';
 const HeaderContainer = styled.div`
     display: flex;
     align-items: center;
-    border: 1px solid black;
     height: 10%;
     font-size: ${(props) => props.theme.fontSize.xxl};
     font-weight: bold;
     justify-content: center;
+    padding: 1.5rem;
+    border-bottom: 1px solid #C2B7DA;
 
     ${(props) => props.theme.device.Tablet} {
         font-size: ${(props) => props.theme.fontSize.xl};
