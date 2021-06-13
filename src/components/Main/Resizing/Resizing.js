a3241b19a1b730725570e7dc08cce365eb1850d0
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-13 17:58:18 +0900

    padding 조절

--- src/components/Main/Resizing/Resizing.js
+++ src/components/Main/Resizing/Resizing.js
@@ -8,7 +8,7 @@ const ResizingContainer = styled.div`
     justify-content: space-between;
     align-items: center;
     width: 40rem;
-    padding: 1rem 0;
+    padding: 2rem 0;
 
     ${(props) => props.theme.device.Tablet} {
         width: 480px;
