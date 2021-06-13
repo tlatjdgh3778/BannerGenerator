3620c99e197a7f74e86257b7bf5cf5afa52ef4db
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-13 16:08:41 +0900

    폰트 변경

--- src/style/global.js
+++ src/style/global.js
@@ -2,7 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 * {
-    font-family: 'Roboto', 'Noto Sans KR', 'Nanum Gothic', 'Merriweather', 'Hammersmith One', 'Source Code Pro', 'Anton', 'Do Hyeon', 'Jua' sans-serif;
+    /* font-family: 'Roboto', 'Noto Sans KR', 'Nanum Gothic', 'Merriweather', 'Hammersmith One', 'Source Code Pro', 'Anton', 'Do Hyeon', 'Jua', sans-serif; */
+    font-family: 'Do Hyeon', 'Roboto' sans-serif;
     margin: 0;
     padding: 0;
 }
@@ -13,9 +14,39 @@ body {
     background-color: white;
 }
 
-/* .MuiInputLabel-root {
-    background-color: white;
-} */
+.sketch-picker {
+    position: absolute;
+    bottom: 3rem;
+    right: -5rem;
+}
+.MuiInputBase-input {
+    font-family: 'Do Hyeon';
+    text-align: center;
+}
+
+// WIDTH, HEIGT LABEL속성
+.MuiFormLabel-root {
+    color: #C2B7DA;
+}
+.MuiFormLabel-root.Mui-focused {
+    color: #C2B7DA;
+}
+
+.MuiOutlinedInput-root {
+    & .MuiOutlinedInput-notchedOutline {
+        border: 2px solid #C2B7DA;
+    }
+    &:hover .MuiOutlinedInput-notchedOutline {
+        border-color: #C2B7DA;
+    }
+    &.Mui-focused .MuiOutlinedInput-notchedOutline {
+        border-color: #C2B7DA;
+    }
+}
+
+.MuiInput-underline {
+    border: none;  
+}
 
 `;
 
