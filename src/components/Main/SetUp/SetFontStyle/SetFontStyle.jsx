0995f175999ece12dd97580f1b21bc84e7688639
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-20 15:12:40 +0900

    Refactor : useContext state와 actions로 구조분해하여 직관적이게

--- src/components/Main/SetUp/SetFontStyle/SetFontStyle.jsx
+++ src/components/Main/SetUp/SetFontStyle/SetFontStyle.jsx
@@ -7,29 +7,29 @@ import * as S from './SetFontStyle.style';
 
 const SetFontStyle = () => {
     console.log('SetFontStyle Render');
-    const { state, actions } = useContext(StyleContext);
+    const { fontStyle, fontSize } = useContext(StyleContext).state;
+    const { setFontStyle, setFontSize } = useContext(StyleContext).actions;
     const fonts = ['Roboto', 'Noto Sans KR', 'Nanum Gothic', 'Merriweather', 'Hammersmith One', 'Source Code Pro', 'Anton', 'Do Hyeon', 'Jua'];
-    const fontSize = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+    const fontSizes = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
     return(
         <S.StyleContainer>
                 <>
                 <FormControl>
                     <Select  
-                    value={state.fontStyle}
-                    onChange={e => {
-                        actions.setFontStyle(e.target.value);}}>
-                        {fonts.map((font, idx) => {
-                            return <MenuItem key={idx} value={font}>{font}</MenuItem>
-                        })}
+                    value={fontStyle}
+                    onChange={e => setFontStyle(e.target.value)}>
+                    {fonts.map((font) => {
+                        return <MenuItem key={font} value={font}>{font}</MenuItem>
+                    })}
                     </Select>
                 </FormControl>
                 <FormControl>
                     <Select 
-                    value={state.fontSize}
-                    onChange={e => actions.setFontSize(e.target.value)}>
-                        {fontSize.map((size, idx) => {
-                            return <MenuItem key={idx} value={size}>{size}</MenuItem>
+                    value={fontSize}
+                    onChange={e => setFontSize(e.target.value)}>
+                        {fontSizes.map((size) => {
+                            return <MenuItem key={size} value={size}>{size}</MenuItem>
                         })}
                     </Select>
                 </FormControl>
