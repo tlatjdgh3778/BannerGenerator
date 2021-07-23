86746862ff1074fce8c7e810b324de3f836c1f77
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-23 23:28:28 +0900

    feat : export actions

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,3 +1,4 @@
 // action 모아두는 곳
 export { addInputText } from './input/actions';
 export { resizeHeight, resizeWidth } from './sizing/actions';
+export { changeFontColor, changeBgColor, changeFontStyle, changeFontSize } from './style/actions';
