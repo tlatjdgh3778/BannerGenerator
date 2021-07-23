6d9c40f68d09676ac51eec16a6d9373cfd3c5b60
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-23 23:28:11 +0900

    feat: reducer 들을 합친 rootReducer 생성

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { sizingReducer } from './sizing/reducer';
 import { inputReducer } from './input/reducer';
+import { styleReducer } from './style/reducer';
 
 const rootReducer = combineReducers({
     sizing: sizingReducer,
     input: inputReducer,
+    style: styleReducer, 
 });
 
 export default rootReducer;
