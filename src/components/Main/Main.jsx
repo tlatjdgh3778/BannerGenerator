a7be58991b966630cfd7722f06bb101b8c4eca75
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-23 23:31:17 +0900

    refactor: context api 에서 redux 로 변경

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,25 +2,16 @@ import React from 'react';
 import Banner from 'components/Main/Banner/Banner';
 import Resizing from 'components/Main/Resizing/Resizing';
 import SetUp from 'components/Main/SetUp/SetUp';
-import { SizingProvider } from 'contexts/sizing';
-import { InputProvider } from 'contexts/input';
-import { StyleProvider } from 'contexts/style';
 import * as S from 'components/Main/Main.style';
 
 const Main = () => {
     console.log('Main Render');
     return(
-        <StyleProvider>
-        <InputProvider>
-        <SizingProvider>
-            <S.MainContainer>
-                <Resizing></Resizing>
-                <Banner></Banner>
-                <SetUp></SetUp>
-            </S.MainContainer>
-        </SizingProvider>
-        </InputProvider>
-        </StyleProvider>   
+        <S.MainContainer>
+            <Resizing></Resizing>
+            <Banner></Banner>
+            <SetUp></SetUp>
+        </S.MainContainer>  
     )
 }
 export default Main;
