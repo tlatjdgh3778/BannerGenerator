2e3ee2f37697890fd7d2e362fd14ce8c19a3aa8e
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-08-09 17:15:32 +0900

    refactor: redux hooks 로 상태관리

--- src/components/Main/Banner/Banner.jsx
+++ src/components/Main/Banner/Banner.jsx
@@ -1,58 +1,36 @@
-import React, { useRef, useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, { useRef, useEffect } from "react";
+import { useSelector } from "react-redux";
 
-const Banner = ({ text, width, height, fontColor, bgColor, fontStyle, fontSize }) => {
-    console.log('Banner Render');
+const Banner = () => {
+    console.log("Banner Render");
+    const text = useSelector(({ input }) => input.text);
+    const width = useSelector(({ sizing }) => sizing.width);
+    const height = useSelector(({ sizing }) => sizing.height);
+    const fontColor = useSelector(({ style }) => style.fontColor);
+    const bgColor = useSelector(({ style }) => style.bgColor);
+    const fontStyle = useSelector(({ style }) => style.fontStyle);
+    const fontSize = useSelector(({ style }) => style.fontSize);
 
     const canvasRef = useRef(null);
-    
+
     useEffect(() => {
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext('2d');
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
 
-            ctx.font = `${fontSize}px ${fontStyle}`;
-            const x = canvas.width / 2;
-            const y = canvas.height / 2;
+        ctx.font = `${fontSize}px ${fontStyle}`;
+        const x = canvas.width / 2;
+        const y = canvas.height / 2;
 
-            ctx.fillStyle = bgColor;
-            ctx.fillRect(0, 0, width, height);
-            ctx.fill();
+        ctx.fillStyle = bgColor;
+        ctx.fillRect(0, 0, width, height);
+        ctx.fill();
 
-            ctx.textAlign = "center";
-            ctx.textBaseline = "middle";
-            ctx.fillStyle = fontColor;
-            ctx.fillText(text, x, y);
-    }, [
-        text,
-        height,
-        width,
-        fontSize,
-        fontStyle,
-        fontColor,
-        bgColor,
-    ])
-    return(
-        <canvas
-            ref={canvasRef}
-            id='canvasBanner'
-            width={width}
-            height={height}
-        ></canvas>
-    );
-}
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = fontColor;
+        ctx.fillText(text, x, y);
+    }, [text, height, width, fontSize, fontStyle, fontColor, bgColor]);
+    return <canvas ref={canvasRef} id="canvasBanner" width={width} height={height}></canvas>;
+};
 
-const mapStateToProps = ({ input, sizing, style }) => {
-    return {
-        text: input.text,
-        width: sizing.width,
-        height: sizing.height,
-        fontColor: style.fontColor,
-        bgColor: style.bgColor,
-        fontStyle: style.fontStyle,
-        fontSize: style.fontSize
-    }
-}
-
-export default connect(
-    mapStateToProps,
-)(Banner);
+export default Banner;
